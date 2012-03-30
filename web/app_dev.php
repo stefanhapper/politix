db82b52fcc529327b8acd78981a4c6a0f8284c59
Stefan Happer <stefan@happer.at>
2012-03-30 19:15:49 +0200

    Allow my IP to access app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,6 +8,7 @@
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
+    '213.49.253.49',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
