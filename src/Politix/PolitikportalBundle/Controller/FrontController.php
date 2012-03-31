be17a05397d8fdfeff85f1dca37be8ff3fad23f2
Stefan Happer <stefan@happer.at>
2012-03-31 21:51:58 +0200

    Try another way to fetch sources from DB

--- src/Politix/PolitikportalBundle/Controller/FrontController.php
+++ src/Politix/PolitikportalBundle/Controller/FrontController.php
@@ -11,13 +11,18 @@ class FrontController extends Controller
     public function indexAction($name)
     {
     
-    	$q = Doctrine_Manager::getInstance()->getCurrentConnection();
-		$result = $q->execute("SELECT name FROM rss_sources");
+    	// $q = Doctrine_Manager::getInstance()->getCurrentConnection();
+		// $result = $q->execute("SELECT name FROM rss_sources");
 		
-		$results = $q->fetchAll();  
+		// $results = $q->fetchAll();  
+
+		
+		$conn = $this->container->get('database_connection');
+		$sql = 'SELECT name FROM rss_sources';
+		$sources = $conn->query($sql);
 
 
-        return $this->render('PolitikportalBundle:Default:index.html.twig', array('name' => $name,'results' => $results));
+        return $this->render('PolitikportalBundle:Default:index.html.twig', array('name' => $name,'sources' => $sources));
         
     }
 }
