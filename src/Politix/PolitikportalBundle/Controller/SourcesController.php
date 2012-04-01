0bc2ae6d81fbb29cf97f3a6204eedbd53f24c5ed
Stefan Happer <stefan@happer.at>
2012-04-01 21:42:03 +0200

    Add SourcesController

---
+++ src/Politix/PolitikportalBundle/Controller/SourcesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Politix\PolitikportalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class FrontController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+		
+		$out['items'] = $this->getSource('tagesschau');
+		$out['name'] = $name;
+		
+        return $this->render('PolitikportalBundle:Default:items.html.twig', $out);
+        
+    }
+    
+    public function myAction($name)
+    {
+		
+		$out['name'] = $name;
+		
+        return $this->render('PolitikportalBundle:Default:my.html.twig', $out);
+        
+    }
+    
+    
+    function getSource($source) {
+    
+    	$out['source'] = $source;
+		$out['name'] = $source;
+
+    	$sql = 'SELECT * FROM rss_items WHERE source LIKE "' . $source . '" ORDER BY pubDate DESC LIMIT 0,10';
+    	return $this->getDB($sql);
+		
+        return $this->render('PolitikportalBundle:Default:items.html.twig', $out);    
+    	
+    }
+    
+    
+    function getDB($sql) {
+    
+    	$conn = $this->container->get('database_connection');
+		return $conn->query($sql);
+    
+    }
+    
+    
+}
