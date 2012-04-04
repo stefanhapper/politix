ab3909ae7335a17f0e18dd8c0762ab01158006f5
Stefan Happer <stefan@happer.at>
2012-04-04 22:47:14 +0200

    Add path when calling object

--- src/Politix/PolitikportalBundle/Controller/SourcesController.php
+++ src/Politix/PolitikportalBundle/Controller/SourcesController.php
@@ -33,7 +33,7 @@ class SourcesController extends Controller {
     function getSourcesAction() {
     
     	require ('/var/www/vhosts/politikportal.eu/subdomains/new/httpdocs/politix/src/Politix/PolitikportalBundle/Model/SourceModel.php');
-    	$db = new SourceModel();
+    	$db = new Politix\PolitikportalBundle\Model\SourceModel();
     	$out['sources'] = $db->getSources;
     	
     	return $this->render('PolitikportalBundle:Default:sources.html.twig', $out);
