82981f675b26fec291aad12cc5b9f724c5275694
Stefan Happer <stefan@happer.at>
2012-04-04 23:19:06 +0200

    Change $this->container-get() to $this->get()

--- src/Politix/PolitikportalBundle/Model/SourceModel.php
+++ src/Politix/PolitikportalBundle/Model/SourceModel.php
@@ -12,7 +12,7 @@ class SourceModel {
     
     public function getSources($start = 0, $max = 10) {
     
-        $conn = $this->container->get('database_connection');
+        $conn = $this->get('database_connection');
 
     	$sql = "SELECT * FROM sources LIMIT $start,$max";
     	
