94071301991e3657356272bff8178799b6043bf2
Stefan Happer <stefan@happer.at>
2012-04-09 13:32:34 +0200

    Activate HTTPCache

--- web/app.php
+++ web/app.php
@@ -2,11 +2,16 @@
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+
+/* cache turned on */
+require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+
+/* cache turned on */
+$kernel = new AppCache($kernel);
+
 $kernel->handle(Request::createFromGlobals())->send();
