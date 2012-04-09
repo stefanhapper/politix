dae19d68546b90e46114b563c61b6718fb5aa6f3
Stefan Happer <stefan@happer.at>
2012-04-09 14:08:09 +0200

    Add heading to MainController and topics template

--- src/Politix/PolitikportalBundle/Controller/MainController.php
+++ src/Politix/PolitikportalBundle/Controller/MainController.php
@@ -14,6 +14,7 @@ class MainController extends Controller {
     	$topics = $TopicModel->getHomepageTopics();
     	
     	$out['rows'][] = 'empty';
+    	$out['heading'] = date('r');
 
     	foreach ($topics as $topic) {
     	
