3bd8dd210d3b25aafd33a5fbedb723c9f38afb20
Stefan Happer <stefan@happer.at>
2012-04-09 21:39:23 +0200

    Add __constructor for MainController, move $response to $this->response, remove testCacheAction

--- src/Politix/PolitikportalBundle/Controller/MainController.php
+++ src/Politix/PolitikportalBundle/Controller/MainController.php
@@ -6,47 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller {
+
+	var $response = FALSE;
+	
+	function __construct() {
+	
+		$this->response = new Response();
+	
+	    $this->response->setPublic();
+		$this->response->setMaxAge(60);
+		$this->response->setSharedMaxAge(60);
+
+	}
+    
     
     public function getHomepageAction() {
 		
-		$ApcLastModified = apc_fetch('homeLastModified');
+		if (apc_fetch('homeLastModified')) {
+		
+			$ApcLastModified = apc_fetch('homeLastModified');
+			
+		} else {
+		
+			$ApcLastModified = 0;
+			
+		}
 
 		$lastModified = new \DateTime('@' . $ApcLastModified);    	
     	
-    	$response = new Response();
-    
-    	$response->setPublic();
-		$response->setMaxAge(60);
-		$response->setSharedMaxAge(60);
+    	$this->response->setLastModified($lastModified);
     	
-    	$response->setLastModified($lastModified);
+	    $request = $this->getRequest();
     	
-    	$request = $this->getRequest();
-    	
-    	/*
-    	
-    	$out['rows'][] = $response->__toString();
-    	$out['rows'][] = $request->__toString();
-    	
-    	$output  = '<pre>' . $out['rows'][0] . '<br><hr><br>';
-    	$output .= $out['rows'][1] . '<br><hr><br>';
-    	$output .= $ApcLastModified . '<br><hr><br>';
-    	$output .= date('r',$ApcLastModified) . '<br><hr><br>';
-    	$output .= $lastModified->format('r') . '</pre>';
-    	
-    	$response->setContent($output);
-    	
-    	return $response;
-    	
-    	return $this->render('PolitikportalBundle:Default:dump.html.twig', $out);
-
-		*/
 		
-    	if ($response->isNotModified($this->getRequest())) {
-    		return $response;
-    	} else {
+    	if ($this->response->isNotModified($this->getRequest())) {
+    		return $this->response;
+    	}
     	
-
+    	
     	$out['heading'] = $lastModified->format('r');
 
         $TopicModel = $this->get('TopicModel');
@@ -54,35 +51,16 @@ class MainController extends Controller {
 
     	$topics = $TopicModel->getHomepageTopics();
     	
-    	$out['rows'][] = 'empty';
+    	// $out['rows'][] = 'empty';
 
     	foreach ($topics as $topic) {
-    	
     		$out['topics'][] = $TopicModel->getTopic($topic);
-    	
     	}
     	
-    	$response->setContent($this->render('PolitikportalBundle:Default:topics.html.twig', $out));
+    	$this->response->setContent($this->render('PolitikportalBundle:Default:topics.html.twig', $out));
 
-    	return $response;    	
-
-        }
+    	return $this->response;    
     	
-    }
-    
-	
-	public function testCacheAction() {
-	
-		$response = new Response(date('r'));
-
-		$response->setPublic();
-
-		$response->setMaxAge(60);
-		$response->setSharedMaxAge(60);
-
-		return $response;
-	
-	}
-        
+    }        
     
 }
