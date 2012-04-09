fb4feacf83871e7248f6b63c99bc08a6aeece74b
Stefan Happer <stefan@happer.at>
2012-04-09 17:11:57 +0200

    Allow access from everywhere to app_dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,6 +6,8 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
+
+/*
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '213.49.253.49',
@@ -19,6 +21,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
+*/
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
