bd7b24c36f36c233f1b3f7f59319296aeee87627
Stefan Happer <stefan@happer.at>
2012-06-26 16:30:25 +0200

    Add partner items to right column

--- src/Politix/PolitikportalBundle/Controller/MainController.php
+++ src/Politix/PolitikportalBundle/Controller/MainController.php
@@ -6,71 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller {
-
+	
 	var $response = FALSE;
 	
 	function __construct() {
-	
 		$this->response = new Response();
-	
-	    $this->response->setPublic();
-		// $this->response->setMaxAge(60);
-		// $this->response->setSharedMaxAge(60);
-		
-		$this->apc = function_exists('apc_fetch');
-		
+	  $this->response->setPublic();
+ // $this->response->setMaxAge(60);
+ // $this->response->setSharedMaxAge(60);
+		$this->apc = function_exists('apc_fetch');	
 	}
     
-    
-    public function getHomepageAction() {
+  public function getHomepageAction() {
 		
 		if (($this->apc) && (apc_fetch('homeLastModified'))) {
-		
 			$ApcLastModified = apc_fetch('homeLastModified');
-			
 		} else {
-		
 			$ApcLastModified = time();
-			
 			if ($this->apc) apc_store('homeLastModified',time());
-			
 		}
-
 		$timezone = new \DateTimeZone('Europe/Brussels');
-		
 		$lastModified = new \DateTime('@' . $ApcLastModified);
-    	$lastModified->setTimezone($timezone);
+    $lastModified->setTimezone($timezone);
+    $this->response->setLastModified($lastModified);
+    $request = $this->getRequest();
+    if ($this->response->isNotModified($this->getRequest())) {
+      return $this->response;
+    }
     	
-    	$this->response->setLastModified($lastModified);
-    	
-	    $request = $this->getRequest();    	
-		
-    	if ($this->response->isNotModified($this->getRequest())) {
-    		return $this->response;
+    $out['heading'] = $lastModified->format('r');
+    
+    $TopicModel = $this->get('TopicModel');
+    $topics = $TopicModel->getHomepageTopics();	
+    $out['topics'] = array();
+    foreach ($topics as $topic) {
+    	if ($items = $TopicModel->getTopic($topic)) {
+    	  $out['topics']['topic'.$topic['id']]['items'] = $items;
+    		$out['topics']['topic'.$topic['id']]['title'] = $topic['title_at'];
     	}
-    	
-    	
-    	$out['heading'] = $lastModified->format('r');
-
-        
-        $TopicModel = $this->get('TopicModel');
-    	
-
-    	$topics = $TopicModel->getHomepageTopics();
-    	
-    	$out['topics'] = array();
-    	
-    	foreach ($topics as $topic) {
-    		if ($items = $TopicModel->getTopic($topic)) {
-    			$out['topics']['topic'.$topic['id']]['items'] = $items;
-    			$out['topics']['topic'.$topic['id']]['title'] = $topic['title_at'];
-    		}
-    	}
-    	
-    	$this->response->setContent($this->renderView('PolitikportalBundle:Default:topics.html.twig', $out));
-
-    	return $this->response;    
-    	
-    }        
+    }
+    
+    $sources = $this->get('SourceModel');
+    $out['partners'][] = $sources->getSource('wkoe',0,1);
+    $this->response->setContent($this->renderView('PolitikportalBundle:Default:topics.html.twig', $out));
+    return $this->response;    
+  }        
     
 }
--- src/Politix/PolitikportalBundle/Model/SourceModel.php
+++ src/Politix/PolitikportalBundle/Model/SourceModel.php
@@ -1,115 +1,68 @@
 <?php
 
 namespace Politix\PolitikportalBundle\Model;
-
 use Doctrine\DBAL\Connection;
-
 
 class SourceModel {
     
-    private $conn;
-    private $pagesize = 100;
+  private $conn;
+  private $pagesize = 100;
     
+  function __construct(Connection $conn) {
+    $this->conn = $conn;
+  }
     
-    function __construct(Connection $conn) {
-    	
-    	$this->conn = $conn;
-    	
-    }
-    
-    
-    public function setPagesize($pagesize) {
-    
-    	$this->pagesize = $pagesize;
-    	
-    }
+  public function setPagesize($pagesize) {
+    $this->pagesize = $pagesize;
+  }
 
+  public function getSources($page = 1) {
+    $start = $this->pagesize * ($page - 1);
+    $sql = "SELECT * FROM rss_sources LIMIT $start,$this->pagesize";
+    return $this->conn->query($sql);
+	}
     
-    public function getSources($page = 1) {
-        
-        $start = $this->pagesize * ($page - 1);
-        
-    	$sql = "SELECT * FROM rss_sources LIMIT $start,$this->pagesize";
-    	
-		return $this->conn->query($sql);
-		    	
+  public function getSource($id, $start = 0, $limit = 10) {
+    $sql = "SELECT * FROM rss_items WHERE source LIKE '$id' ORDER BY pubDate DESC LIMIT $start,$limit";
+    return $this->conn->fetchAll($sql);
+  }
+    
+  public function addSid() {
+    $sql = "SELECT id,sid FROM rss_sources";
+    $result = $this->conn->fetchAll($sql);
+    $sources = array();
+    foreach ($result as $source) {
+    	$sources[$source['id']]['sid'] = $source['sid'];
+    	$sources[$source['id']]['source'] = $source['id'];
+    	$sources[$source['id']]['count'] = 0;
     }
-    
-    
-    public function getSource($id) {
-    
-    	$sql = "SELECT * FROM rss_items WHERE source LIKE '" . $id . "' ORDER BY pubDate DESC LIMIT 0,10";
-    	
-		return $this->conn->query($sql);
-    
-    }
-    
-    public function addSid() {
-    
-    	$sql = "SELECT id,sid FROM rss_sources";
-    	
-    	$result = $this->conn->fetchAll($sql);
-    	
-    	$sources = array();
-    	
-    	foreach ($result as $source) {
-    		$sources[$source['id']]['sid'] = $source['sid'];
-    		$sources[$source['id']]['source'] = $source['id'];
-    		$sources[$source['id']]['count'] = 0;
+    $sql = "SELECT id,source FROM rss_items WHERE id > 170000";
+    $items = $this->conn->fetchAll($sql);
+    foreach ($items as $item) {
+  		$sql = "UPDATE rss_items SET sid = " . $sources[$item['source']]['sid'] . " WHERE id = " . $item['id'];
+  		$sources[$item['source']]['count'] += 1;
+  	}
+  	return $sources;
+  }
+
+  public function getPages($page = 1) {
+    $sql = "SELECT COUNT(id) AS num FROM rss_sources";
+    $result = $this->conn->fetchAssoc($sql);
+    $totalpages = round($result['num'] / $this->pagesize);
+    for ($i=1;$i <= $totalpages;$i++) {
+      if (($i == 1) or (($i - $page > -5) and ($i - $page < 5)) or ($i == $totalpages)) {
+    	  $current = FALSE;
+    	  $disabled = FALSE;
+    	  if ($page == $i) {
+    	    $current = TRUE;
+    		  $disabled = TRUE;
+    	  }
+        $pages[] = array('number' => $i, 'current' => $current, 'disabled' => $disabled);
+      } else {
+    	  if (($i - $page == -6) or ($i - $page == 6)) $pages[] = array('number' => '…', 'current' => FALSE, 'disabled' => TRUE);
     	}
-    	
-    	
-    	$sql = "SELECT id,source FROM rss_items WHERE id > 170000";
-    
-    	$items = $this->conn->fetchAll($sql);
+    }
+    return $pages;    	
+  }
   
-  		foreach ($items as $item) {
-  		
-  			$sql = "UPDATE rss_items SET sid = " . $sources[$item['source']]['sid'] . " WHERE id = " . $item['id'];
-  			
-  			$sources[$item['source']]['count'] += 1;
-  			
-  		}
-  		
-  		return $sources;
-    	
-    }
-
-
-    public function getPages($page = 1) {
-                
-    	$sql = "SELECT COUNT(id) AS num FROM rss_sources";
-    	
-    	$result = $this->conn->fetchAssoc($sql);
-    	
-    	$totalpages = round($result['num'] / $this->pagesize);
-    	    	
-    	for ($i=1;$i <= $totalpages;$i++) {
-    	
-    		if (($i == 1) or (($i - $page > -5) and ($i - $page < 5)) or ($i == $totalpages)) {
-    		
-    			$current = FALSE;
-    			$disabled = FALSE;
-    			if ($page == $i) {
-    				$current = TRUE;
-    				$disabled = TRUE;
-    			}
-    			
-    			$pages[] = array('number' => $i, 'current' => $current, 'disabled' => $disabled);
-    		
-    		} else {
-    		
-    			if (($i - $page == -6) or ($i - $page == 6)) $pages[] = array('number' => '…', 'current' => FALSE, 'disabled' => TRUE);
-    		
-    		}
-    		
-    	}
-    	
-    	return $pages;
-		    	
-    }
-    
-    
-    
-    
 }
