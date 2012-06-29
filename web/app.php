03482d31e1768b4d44d074ad22102f5a9df34989
Stefan Happer <stefan@happer.at>
2012-06-29 02:04:13 -0700

    Updated app.php

--- web/app.php
+++ web/app.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-/* cache turned on */
+/* yes, cache turned on */
 $kernel = new AppCache($kernel);
 
 $kernel->handle(Request::createFromGlobals())->send();
