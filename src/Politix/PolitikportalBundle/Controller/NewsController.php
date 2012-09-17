1e81abb5b733f28b31215a8c47a288e6b1971e48
Stefan Happer <stefan@happer.at>
2012-09-17 21:34:48 +0200

    Update routing, NewsController and NewsModel to handle redirect query with optional subscriber id

--- src/Politix/PolitikportalBundle/Controller/NewsController.php
+++ src/Politix/PolitikportalBundle/Controller/NewsController.php
@@ -4,19 +4,66 @@ namespace Politix\PolitikportalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class NewsController extends Controller {
   
-  public function getLinkAction($id) {
-    $NewsModel = $this->get('NewsModel');
-    $url = $NewsModel->getLink($id);
-    if ($url) {
-      $response = new RedirectResponse($url);
+  var $id;
+  var $url;
+  var $querystring;
+  var $subscriber;
+  var $request;
+  var $cookie;
+  var $model;
+  
+  function __construct() {
+    $this->request = Request::createFromGlobals();
+    if ($this->request->cookies->get('i')) {
+      $subscriber = $request->cookies->get('i');
+    }
+  }
+  
+  public function gotoNewsAction($querystring) {
+    $this->model = $this->get('NewsModel');
+    $this->querystring = $querystring;
+    $this->getQueryParams();
+    $this->setSubscriberCookie();
+    $this->getLink();
+    return $this->createResponse();
+  }
+  
+  public function getQueryParams() {
+    if (strstr($this->querystring,'O')) {
+      $queryParams = explode('O',$this->querystring);
+      $this->subscriber = $queryParams[0];
+      $this->id = $queryParams[1];
+    } else {
+      $this->id = $this->querystring;
+    }
+  }
+  
+  public function setSubscriberCookie() {
+    if ($this->subscriber) {
+      $this->cookie = new Cookie('i',$this->subscriber,time()+60*60*24*999,'/','.politikportal.eu');
+    }
+  }
+  
+  public function getLink() {
+    $this->url = $this->model->getLink($this->id);
+  }
+   
+  public function createResponse() {
+    if ($this->url) {
+      $p = 0;
+      $this->model->saveClick($this->id,$this->url,$this->subscriber,$p,0);
+      $response = new RedirectResponse($this->url);
+      if ($this->cookie) $response->headers->setCookie($this->cookie);
       $response->headers->set('cache-control','no-cache, no-store, must-revalidate');
     } else {
       throw $this->createNotFoundException('Der Artikel konnte nicht gefunden werden');
     }
     return $response;
   }
-    
+   
 }
--- src/Politix/PolitikportalBundle/Model/NewsModel.php
+++ src/Politix/PolitikportalBundle/Model/NewsModel.php
@@ -21,5 +21,30 @@ class NewsModel {
       return FALSE;
     }
   }
+  
+  public function saveClick($id, $url, $i, $p, $textversion) {
+    if (isset($_SERVER["HTTP_REFERER"])) {
+      $referer = "'" . $_SERVER["HTTP_REFERER"] . "'";
+    } else {
+      $referer = "NULL";
+    }
+    $sql = "INSERT INTO clicklog01
+              (id,site,ip,user,url,useragent,go,ref,item_id,i,p,sessionid,textversion)
+		          VALUES (
+		            NULL," .
+		            "'politikportal'," .
+		            "'" . $_SERVER["REMOTE_ADDR"] . "'," . 
+					      "NULL," . 
+					      "'" . $_SERVER["REQUEST_URI"] . "'," . 
+					      "'" . $_SERVER["HTTP_USER_AGENT"] . "'," .
+					      "'" . $url . "'," .
+					      $referer . "," .
+					      $id . "," .
+					      $i . "," .
+					      "'" . $p . "'," .
+					      "'" . session_id() . "'," . 
+					      $textversion . ")";
+    $this->conn->query($sql);
+  }
 
 }
